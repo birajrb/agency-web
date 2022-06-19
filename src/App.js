3fc912f51ed47132d65732da2c164be247440729
= <=>
2022-06-19 19:55:03 +0545

    Adds navbar and homepage skeleton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./views/Home";
+
 function App() {
-  return <div>sdfsdf</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
